443c89f5a158170709d21f422e7db5b66e8e02c0
Shad Matheny <220810537+srmatheny@users.noreply.github.com>
2025-08-27 20:17:23 -0700

    project complete, comments not really sufficient

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 console.log("Hello World!");
 
-let userScore = 0;
-let compScore = 0;
-let rndWinner = "a";
+let gameWinner = "z";
+//let rndWinner = "a";
 
 
 function getComputerChoice()    {
@@ -42,52 +41,89 @@ function getUserChoice()    {
 
 //let userChoice = getUserChoice();
 //console.log(userChoice);
+function playGame(numRnds) {
 
-function playRound(userChoice, compChoice)  {
+    let userScore = 0;
+    let compScore = 0;
+    let winner = "a";
+    let rndWinner = "b";
 
-    if ((userChoice.toLowerCase() === 'rock') && (compChoice.toLowerCase() === 'rock')){
-            console.log("Tie! Play Again!");
-            return "t";
-    }   else if ((userChoice.toLowerCase() === 'rock') && (compChoice.toLowerCase() === 'paper')){
-            console.log("You Lose! Paper beats Rock. Better luck next time!");
-            return "c";
-    }   else if ((userChoice.toLowerCase() === 'rock') && (compChoice.toLowerCase() === 'scissors')){
-            console.log("You Win! Rock beats Scissors.");
-            return "u";
-    }   else if ((userChoice.toLowerCase() === 'paper') && (compChoice.toLowerCase() === 'rock')) {
-            console.log("You Win! Paper beats Rock.");
-            return "u";
-    }   else if ((userChoice.toLowerCase() === 'paper') && (compChoice.toLowerCase() === 'paper'))  {
-            console.log("Tie! Play Again!");
-            return "t";
-    }   else if ((userChoice.toLowerCase() === 'paper') && (compChoice.toLowerCase() === 'scissors'))  {
-            console.log("You Lose! Scissors beats Paper. Better luck next time!");
-            return "c";
-    }   else if ((userChoice.toLowerCase() === 'scissors') && (compChoice.toLowerCase() === 'rock'))  {
-            console.log("You Lose! Rock beats Scissors. Better luck next time!");
-            return "c";
-    }   else if ((userChoice.toLowerCase() === 'scissors') && (compChoice.toLowerCase() === 'paper'))  {
-            console.log("You Win! Scissors beats Paper.");
-            return "u";
-    }   else if ((userChoice.toLowerCase() === 'scissors') && (compChoice.toLowerCase() === 'scissors'))  {
-            console.log("Tie! Play Again!");
-            return "t"
-    }   else{
-        return "Something starnge wackadoodle happened..."
-    }
+    function playRound(userChoice, compChoice)  {
+
+        if ((userChoice.toLowerCase() === 'rock') && (compChoice.toLowerCase() === 'rock')){
+                console.log("Tie! Play Again!");
+                return "t";
+        }   else if ((userChoice.toLowerCase() === 'rock') && (compChoice.toLowerCase() === 'paper')){
+                //console.log("You Lose! Paper beats Rock. Better luck next time!");
+                return "c";
+        }   else if ((userChoice.toLowerCase() === 'rock') && (compChoice.toLowerCase() === 'scissors')){
+                //console.log("You Win! Rock beats Scissors.");
+                return "u";
+        }   else if ((userChoice.toLowerCase() === 'paper') && (compChoice.toLowerCase() === 'rock')) {
+                //console.log("You Win! Paper beats Rock.");
+                return "u";
+        }   else if ((userChoice.toLowerCase() === 'paper') && (compChoice.toLowerCase() === 'paper'))  {
+                console.log("Tie! Play Again!");
+                return "t";
+        }   else if ((userChoice.toLowerCase() === 'paper') && (compChoice.toLowerCase() === 'scissors'))  {
+                //console.log("You Lose! Scissors beats Paper. Better luck next time!");
+                return "c";
+        }   else if ((userChoice.toLowerCase() === 'scissors') && (compChoice.toLowerCase() === 'rock'))  {
+                //console.log("You Lose! Rock beats Scissors. Better luck next time!");
+                return "c";
+        }   else if ((userChoice.toLowerCase() === 'scissors') && (compChoice.toLowerCase() === 'paper'))  {
+                //console.log("You Win! Scissors beats Paper.");
+                return "u";
+        }   else if ((userChoice.toLowerCase() === 'scissors') && (compChoice.toLowerCase() === 'scissors'))  {
+                console.log("Tie! Play Again!");
+                return "t"
+        }   else{
+            return "Something strange wackadoodle happened..."
+        }
     // do stuff in here to play the round
+    }
 
+    for (i=1; i<=numRnds; i++)   {
+
+        const userChoice = getUserChoice();
+        //console.log(userChoice);
+        const compChoice = getComputerChoice();
+        //console.log(compChoice);
+
+        rndWinner = (playRound(userChoice, compChoice));
+    
+        if (rndWinner === 't')  {
+                i--;
+        }   else if (rndWinner === 'u') {
+                userScore++;
+        }   else    {
+                compScore++;
+        }
+
+        
+
+    }
+    
+    if (userScore === compScore)  {
+        winner = "Tie! You ran an even number of games."
+    } else if (userScore > compScore)   {
+        winner = "User wins!   " + userScore + " to " + compScore;
+    } else  {
+        winner = "Computer wins!   " + compScore + " to " + userScore;
+    }
+    
+    //console.log(winner);
+    //console.log(userScore);
+    //console.log(compScore);
+
+    return winner;
 }
 
-const userChoice = getUserChoice();
-console.log(userChoice);
-const compChoice = getComputerChoice();
-console.log(compChoice);
+    
+gameWinner = playGame(5);
+console.log(gameWinner);
 
-console.log(rndWinner);
-rndWinner = (playRound(userChoice, compChoice));
-console.log(rndWinner);
 
-console.log(userScore);
-console.log(compScore);
-
+    
+    
+    
